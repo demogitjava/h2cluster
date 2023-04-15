9d6a627417e9d0d0d6e66e186c2e742acc12a97d
hoscho <jgeist@gmx.net>
2023-04-15 15:10:46 +0200

    init commit

---
+++ src/main/java/de/jgsoftware/h2cluster/h2cluster.java
@@ -0,0 +1,13 @@
+package de.jgsoftware.h2cluster;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author hoscho
+ */
+@SpringBootApplication
+public class h2cluster implements ih2cluster
+{
+}
