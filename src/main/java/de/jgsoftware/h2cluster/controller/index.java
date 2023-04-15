91bfa9b60d2056f86f59e553738545ea70804130
hoscho <jgeist@gmx.net>
2023-04-15 15:44:35 +0200

    edit docker command to cpu 1

---
+++ src/main/java/de/jgsoftware/h2cluster/controller/index.java
@@ -0,0 +1,31 @@
+package de.jgsoftware.h2cluster.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+import de.jgsoftware.h2cluster.controller.interfaces.ih2index;
+
+/**
+ *
+ * @author hoscho
+ */
+
+@Controller
+public class index implements ih2index
+{
+
+    ModelAndView mv;
+
+    @Override
+    public ModelAndView index()
+    {
+        mv = new ModelAndView("index");
+
+        return mv;
+    }
+}
