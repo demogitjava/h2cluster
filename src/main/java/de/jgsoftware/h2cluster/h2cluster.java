11296f81254ce9cbfdc20b920c4eac60e3d301c3
hoscho <jgeist@gmx.net>
2023-04-16 08:18:05 +0200

    edit pom.xml

--- src/main/java/de/jgsoftware/h2cluster/h2cluster.java
+++ src/main/java/de/jgsoftware/h2cluster/h2cluster.java
@@ -17,25 +17,5 @@ import java.time.format.DateTimeFormatter;
 public class h2cluster
 {
 
-    public static String st_timezones = "Europe/Berlin";
 
-    static void main(String[] args) {
-        ZonedDateTime now = ZonedDateTime.now(ZoneId.of(st_timezones));
-        //ZonedDateTime now = ZonedDateTime.now();
-
-        // set up to GMT
-        now.withZoneSameInstant(ZoneId.of("UTC"))
-                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        // 1. ZonedDateTime to TimeStamp
-        Timestamp timestamp = Timestamp.valueOf(now.toLocalDateTime());
-
-        // 2. ZonedDateTime to TimeStamp , no different
-        Timestamp timestamp2 = Timestamp.from(now.toInstant());
-
-        System.out.println(now);
-        System.out.println(timestamp);
-        System.out.println(timestamp2);
-
-        SpringApplication.run(h2cluster.class, args);
-    }
 }
---
+++ src/main/java/de/jgsoftware/h2cluster/i_h2cluster.java
@@ -0,0 +1,39 @@
+package de.jgsoftware.h2cluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+/**
+ *
+ * @author hoscho
+ */
+@SpringBootApplication
+public interface i_h2cluster
+{
+
+    public static String st_timezones = "Europe/Berlin";
+
+    static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of(st_timezones));
+        //ZonedDateTime now = ZonedDateTime.now();
+
+        // set up to GMT
+        now.withZoneSameInstant(ZoneId.of("UTC"))
+                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        // 1. ZonedDateTime to TimeStamp
+        Timestamp timestamp = Timestamp.valueOf(now.toLocalDateTime());
+
+        // 2. ZonedDateTime to TimeStamp , no different
+        Timestamp timestamp2 = Timestamp.from(now.toInstant());
+
+        System.out.println(now);
+        System.out.println(timestamp);
+        System.out.println(timestamp2);
+
+        SpringApplication.run(h2cluster.class, args);
+    }
+}
