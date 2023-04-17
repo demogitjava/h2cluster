b224de39111343447713253de540ad1f69d27e9e
hoscho <jgeist@gmx.net>
2023-04-17 11:31:26 +0200

    edit port for tomcat to 8080

--- src/main/java/de/jgsoftware/h2cluster/controller/interfaces/ih2index.java
+++ src/main/java/de/jgsoftware/h2cluster/controller/interfaces/ih2index.java
@@ -1,8 +1,16 @@
 package de.jgsoftware.h2cluster.controller.interfaces;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
+/**
+ *
+ * @author hoscho
+ */
+
+@RequestMapping("/")
 public interface ih2index
 {
 
